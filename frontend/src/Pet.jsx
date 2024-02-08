ff79f26e64dcc7255902e72aadeda498db2ee360
catbus00 <catmousekill@gmail.com>
2024-02-08 18:21:32 +0700

    update select fields pets

--- frontend/src/Pet.jsx
+++ frontend/src/Pet.jsx
@@ -1,5 +1,15 @@
+import {
+  Button,
+  Select,
+  MenuItem,
+  Typography,
+  TextField,
+  FormControl,
+  InputLabel,
+} from "@mui/material";
+import VerticalBox from "./components/VerticalBox";
 import { useState } from "react";
-import { useForm, useFieldArray, useWatch } from "react-hook-form";
+import { useForm, useFieldArray, Controller } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 
 // Add Pet Function
@@ -50,107 +60,145 @@ function AddPet({ user }) {
   return (
     <>
       <form onSubmit={(e) => handleSubmit(onSubmit)(e)}>
-        <label>Pet Name</label>
-        <input
-          {...register("name", {
-            required: "The name of your pet is required.",
-            maxLength: {
-              value: 50,
-              message: "The maximum length is 50 characters.",
-            },
-          })}
-          placeholder="Pet Name"
-        />
-        {errors.name?.message}
-        <label>Choose Gender</label>
-        <select {...register("gender")}>
-          <option value="male">male</option>
-          <option value="female">female</option>
-          <option value="other">other</option>
-        </select>
-        <label>Color</label>
-        <input
-          {...register("color", {
-            required: "The color of your pet is required.",
-            maxLength: {
-              value: 100,
-              message: "The maximum length is 100 characters.",
-            },
-          })}
-          placeholder="Pet Color"
-        />
-        {errors.color?.message}
-        <label>Age</label>
-        <input
-          type="number"
-          {...register("age", {
-            required: "The age of your pet is required.",
-            maxLength: {
-              value: 3,
-              message: "The maximum length is 3 characters",
-            },
-          })}
-        />
-        {errors.age?.message}
-        {likesField.map((field, index) => {
-          return (
-            <section key={field.id}>
-              <label>
-                <span>Likes</span>
-                <input {...register(`likes[${index}].name`)} />
-              </label>
-              <button
-                type="button"
-                onClick={() => {
-                  likesRemove(index);
-                }}
-              >
-                Delete
-              </button>
-            </section>
-          );
-        })}
-        <button
-          type="button"
-          onClick={() => {
-            likesAppend({ name: "" });
-          }}
-        >
-          Add Likes
-        </button>
-        {dislikesField.map((field, index) => {
-          return (
-            <section key={field.id}>
-              <label>
-                <span>Dislikes</span>
-                <input {...register(`dislikes[${index}].name`)} />
-              </label>
-              <button
-                type="button"
-                onClick={() => {
-                  dislikesRemove(index);
-                }}
-              >
-                Delete
-              </button>
-            </section>
-          );
-        })}
-        <button
-          type="button"
-          onClick={() => {
-            dislikesAppend({ name: "" });
-          }}
-        >
-          Add Dislikes
-        </button>
-        <label>Choose Species</label>
-        <select {...register("species")}>
-          <option value="cat">cat</option>
-          <option value="dog">dog</option>
-          <option value="other">other</option>
-        </select>
-        <button type="submit">Submit</button>
+        <VerticalBox>
+          <Controller
+            control={control}
+            name="name"
+            rules={{
+              required: "The name of your pet is required.",
+              maxLength: {
+                value: 50,
+                message: "The maximum length is 50 characters.",
+              },
+            }}
+            render={({ field }) => (
+              <TextField
+                {...field}
+                label="Pet Name"
+                id="name"
+                placeholder="Pet Name"
+                error={Boolean(errors.name)}
+              />
+            )}
+          ></Controller>
+          <Typography>{errors.name?.message}</Typography>
+          <FormControl>
+            <InputLabel>Choose Gender</InputLabel>
+            <Controller
+              control={control}
+              name="gender"
+              render={({ field }) => (
+                <Select {...field} id="gender" sx={{ width: "200px" }}>
+                  <MenuItem key="male" value="male">
+                    male
+                  </MenuItem>
+                  <MenuItem key="female" value="female">
+                    female
+                  </MenuItem>
+                  <MenuItem key="other" value="other">
+                    other
+                  </MenuItem>
+                </Select>
+              )}
+            />
+          </FormControl>
+          <Controller
+            control={control}
+            name="color"
+            rules={{
+              required: "The color of your pet is required.",
+              maxLength: {
+                value: 100,
+                message: "The maximum length is 100 characters.",
+              },
+            }}
+            render={({ field }) => (
+              <TextField
+                {...field}
+                label="Color"
+                id="color"
+                placeholder="Pet Color"
+                error={Boolean(errors.color)}
+              />
+            )}
+          ></Controller>
+          <Typography>{errors.color?.message}</Typography>
+          <label>Age</label>
+          <input
+            type="number"
+            {...register("age", {
+              required: "The age of your pet is required.",
+              maxLength: {
+                value: 3,
+                message: "The maximum length is 3 characters",
+              },
+            })}
+          />
+          <Typography>{errors.age?.message}</Typography>
+          <VerticalBox>
+            {likesField.map((field, index) => {
+              return (
+                <section key={field.id}>
+                  <label>
+                    <span>Likes</span>
+                    <input {...register(`likes[${index}].name`)} />
+                  </label>
+                  <Button
+                    type="Button"
+                    onClick={() => {
+                      likesRemove(index);
+                    }}
+                  >
+                    Delete
+                  </Button>
+                </section>
+              );
+            })}
+          </VerticalBox>
+          <Button
+            type="Button"
+            onClick={() => {
+              likesAppend({ name: "" });
+            }}
+          >
+            Add Likes
+          </Button>
+          <VerticalBox>
+            {dislikesField.map((field, index) => {
+              return (
+                <section key={field.id}>
+                  <label>
+                    <span>Dislikes</span>
+                    <input {...register(`dislikes[${index}].name`)} />
+                  </label>
+                  <Button
+                    type="Button"
+                    onClick={() => {
+                      dislikesRemove(index);
+                    }}
+                  >
+                    Delete
+                  </Button>
+                </section>
+              );
+            })}
+          </VerticalBox>
+          <Button
+            type="Button"
+            onClick={() => {
+              dislikesAppend({ name: "" });
+            }}
+          >
+            Add Dislikes
+          </Button>
+          <label>Choose Species</label>
+          <Select {...register("species")} value="cat">
+            <MenuItem value="cat">cat</MenuItem>
+            <MenuItem value="dog">dog</MenuItem>
+            <MenuItem value="other">other</MenuItem>
+          </Select>
+          <Button type="submit">Submit</Button>
+        </VerticalBox>
       </form>
     </>
   );
