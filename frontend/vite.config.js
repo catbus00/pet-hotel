fdc0a4d0dd0e145ff2fd48cc9f08ba6adda5ec08
catbus00 <catmousekill@gmail.com>
2024-02-12 22:39:19 +0700

    Configure Vite for production

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,11 +2,16 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 import path from "path";
+import "dotenv/config";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svgr(), react()],
   root: ".",
+  server: {
+    host: process.env.DOMAIN,
+    port: process.env.PORT,
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
