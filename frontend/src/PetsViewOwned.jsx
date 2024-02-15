b129298a35bd61edf5da694000477eab8cc65894
catbus00 <catmousekill@gmail.com>
2024-02-15 14:09:14 +0700

    remove unused features

--- frontend/src/PetsViewOwned.jsx
+++ frontend/src/PetsViewOwned.jsx
@@ -4,10 +4,8 @@ import { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import {
   Card,
-  CardActions,
   CardContent,
   CardMedia,
-  Button,
   Typography,
   Box,
   List,
