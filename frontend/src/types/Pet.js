7467d873bb863e5591239fa5dc48adde797621b3
catbus00 <catmousekill@gmail.com>
2024-02-16 16:05:28 +0700

    update pets prop type

--- frontend/src/types/Pet.js
+++ frontend/src/types/Pet.js
@@ -2,11 +2,20 @@ import PropTypes from "prop-types";
 
 export const Pet = {
   name: PropTypes.string.isRequired,
-  nickname: PropTypes.string.isRequired,
-  photo: PropTypes.string,
-  status: PropTypes.oneOf(["anonymous", "customer", "owner"]),
+  gender: PropTypes.string.isRequired,
+  color: PropTypes.string.isRequired,
+  age: PropTypes.number.isRequired,
   likes: PropTypes.arrayOf(PropTypes.string),
   dislikes: PropTypes.arrayOf(PropTypes.string),
-  species: PropTypes.oneOf(["human", "cat", "dog"]),
-  gender: PropTypes.oneOf(["male", "female"]),
+  species: PropTypes.oneOf(["cat", "dog", "other"]).isRequired,
+  hotel: PropTypes.shape({
+    _bsontype: PropTypes.oneOf(["ObjectID"]).isRequired,
+    id: PropTypes.string.isRequired,
+  }).isRequired,
+  photo: PropTypes.string,
+};
+
+export const Pets = {
+  pets: PropTypes.arrayOf(PropTypes.shape(Pet)),
+  setPets: PropTypes.func,
 };
