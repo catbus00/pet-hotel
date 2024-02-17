9be44be53d32a4f96ba750d7f03851149f16c105
catbus00 <catmousekill@gmail.com>
2024-02-17 16:48:19 +0700

    port

--- frontend/src/env.jsx
+++ frontend/src/env.jsx
@@ -1,4 +1,11 @@
+// const DOMAIN =
+//   import.meta.env.VITE_DOMAIN || "https://meowtel-backend.onrender.com";
+// const PORT = import.meta.env.VITE_PORT || 10000;
+// export const API = `${DOMAIN}:${PORT}`;
+
 const DOMAIN =
   import.meta.env.VITE_DOMAIN || "https://meowtel-backend.onrender.com";
-const PORT = import.meta.env.VITE_PORT || 10000;
+const PORT = import.meta.env.VITE_PORT
+  ? parseInt(import.meta.env.VITE_PORT)
+  : 10000;
 export const API = `${DOMAIN}:${PORT}`;
