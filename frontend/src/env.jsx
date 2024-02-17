d88c22cfd1c7021fa2692f4e8053697d0b80c05f
catbus00 <catmousekill@gmail.com>
2024-02-17 19:06:42 +0700

    Improve env

--- frontend/src/env.jsx
+++ frontend/src/env.jsx
@@ -1,11 +1,7 @@
-// const DOMAIN =
-//   import.meta.env.VITE_DOMAIN || "https://meowtel-backend.onrender.com";
-// const PORT = import.meta.env.VITE_PORT || 10000;
-// export const API = `${DOMAIN}:${PORT}`;
-
 const DOMAIN =
   import.meta.env.VITE_DOMAIN || "https://meowtel-backend.onrender.com";
-const PORT =
-  (import.meta.env.VITE_PORT && import.meta.env.VITE_PORT !== "") ??
-  parseInt(import.meta.env.VITE_PORT);
-export const API = PORT ? `${DOMAIN}:${PORT}` : DOMAIN;
+const PORT = import.meta.env.VITE_PORT
+  ? parseInt(import.meta.env.VITE_PORT)
+  : undefined;
+export const API =
+  PORT && typeof PORT === "number" ? `${DOMAIN}:${PORT}` : DOMAIN;
